4661f2e578f3fd0c3dabb06938285475bbf7a988
jagdish deore <jagdishdeore7767@gmail.com>
2024-03-21 17:47:30 +0530

    Add project files.

---
+++ Calculator/Controllers/CalController.cs
@@ -0,0 +1,52 @@
+﻿using Calculator.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalController : Controller
+    {
+        // GET: Cal
+        public ActionResult Index()
+        {
+            return View(new Cal());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Cal c, string calculator)
+        {
+
+
+            if (calculator == "add")
+            {
+                c.total = c.no1 + c.no2;
+            }
+            else if (calculator == "min")
+            {
+                c.total = c.no1 - c.no2;
+            }
+            else if (calculator == "mul")
+            {
+                c.total = c.no1 * c.no2;
+            }
+            else if (calculator == "div")
+            {
+                c.total = c.no1 / c.no2;
+            }
+
+
+
+            //try
+            //{
+            //    c.total = c.no1 / c.no2;
+
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+
+            return View(c);
+        }
+    }
+}
---
+++ Calculator/Models/Cal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class Cal
+    {
+        public int no1 { get; set; }
+        public int no2 { get; set; }
+        public int total { get; set; }
+
+    }
+}
